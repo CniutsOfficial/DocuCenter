046250491ec54a727b99dd7b47129503e2595e1a
CniutsOfficial <cniuts@outlook.com>
2025-04-11 09:58:21 +0800

    --

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/DocuCenter/',
   lang: 'zh-CN',
   title: 'DocuCenter',
   description: '初桐小栈的文档中心',
